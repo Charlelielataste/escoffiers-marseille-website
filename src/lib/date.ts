990d04def45223e9517d544d43dc68afeaa5090c
Charlelielataste <latastecharlelie@gmail.com>
2025-08-29 17:55:16 +0200

    feat(components): update page and components for sanity

--- src/lib/date.ts
+++ src/lib/date.ts
@@ -8,3 +8,16 @@ export function formatDate(dateString: Date, locale: Locale) {
     day: "numeric",
   });
 }
+
+export function formatDateTime(dateString: Date) {
+  const date = new Date(dateString);
+  const dateFormatted = date.toLocaleDateString("fr-FR", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
+  const hours = date.getHours().toString().padStart(2, "0");
+  const minutes = date.getMinutes().toString().padStart(2, "0");
+  const timeFormatted = `${hours}h${minutes}`;
+  return `${dateFormatted} à ${timeFormatted}`;
+}
